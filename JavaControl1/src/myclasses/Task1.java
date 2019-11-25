04c866d3b34e0dec47b69e57aa7da5db313641cd
AlexRussovits <sasikrus2002@gmail.com>
2019-11-25 23:36:56 +0200

    Зубчатый массив

--- JavaControl1/src/myclasses/Task1.java
+++ JavaControl1/src/myclasses/Task1.java
@@ -15,7 +15,7 @@ import java.util.Scanner;
  */
 public class Task1 {
     public void run (){
-    //Scanner scanner = new Scanner (System.in);
+    Scanner scanner = new Scanner (System.in);
     int [][] Weather = new int [12][];           
     Weather[0] =  new int[31];
     Weather[1] =  new int[29];
@@ -31,10 +31,188 @@ public class Task1 {
     Weather[11] =  new int[31];
     Random rnd = new Random();
     for (int i= 0;i<Weather.length;i++) {
-        for (int j=0; j< Weather.length;j++) {    
-           Weather [i][j] = rnd.nextInt(30);
-           System.out.println(Weather[i][j]+", ");
+        switch (i) {
+            case 0:
+            case 1:
+            case 11:
+                switch (Weather[i].length) {
+                    case 30:
+                        for(int j = 0; j < 30; j++) {
+                            Weather[i][j] = rnd.nextInt(16+(-12+1)+(-16));
+                        }   break;
+                    case 31:
+                        for (int j = 0; j < 31; j++) {
+                            Weather[i][j] = rnd.nextInt(16+(-12+1)+(-16));
+                        }   break;
+                    case 29:
+                        for (int j = 0; j < 29; j++) {
+                            Weather[i][j] = rnd.nextInt(16+(-13+1)+(-13));
+                        }   break;
+                    default:
+                        break;
+                }   break;
+            case 2:
+            case 3:
+            case 4:
+                switch (Weather[i].length) {
+                    case 30:
+                        for(int j = 0; j < 30; j++) {
+                            Weather[i][j] = rnd.nextInt(22-(6+1)+ 6);
+                        }   break;
+                    case 31:
+                        for (int j = 0; j < 31; j++) {
+                            Weather[i][j] = rnd.nextInt(22-(6+1)+ 6);
+                        }   break;
+                    case 29:
+                        for (int j = 0; j < 29; j++) {
+                            Weather[i][j] = rnd.nextInt(22-(6+1)+ 6);
+                        }   break;
+                    default:
+                        break;
+                }  break;
+            case 5:
+            case 6:
+            case 7:
+                switch (Weather[i].length) {
+                    case 30:
+                        for(int j = 0; j < 30; j++) {
+                            Weather[i][j] = rnd.nextInt(25-(6+1)+ 6);
+                        }   break;
+                    case 31:
+                        for (int j = 0; j < 31; j++) {
+                            Weather[i][j] = rnd.nextInt(25-(6+1)+ 6);
+                        }   break;
+                    case 29:
+                        for (int j = 0; j < 29; j++) {
+                            Weather[i][j] = rnd.nextInt(25-(6+1)+ 6);
+                        }   break;
+                    default:
+                        break;
+                }  break;
+            case 8:
+            case 9:
+            case 10:
+                switch (Weather[i].length) {
+                    case 30:
+                        for(int j = 0; j < 30; j++) {
+                            Weather[i][j] = rnd.nextInt(14-(0+1)+ 3);
+                        }   break;
+                    case 31:
+                        for (int j = 0; j < 31; j++) {
+                            Weather[i][j] = rnd.nextInt(14-(0+1)+ 3);
+                        }   break;
+                    case 29:
+                        for (int j = 0; j < 29; j++) {
+                            Weather[i][j] = rnd.nextInt(14-(0+1)- 3);
+                        }   break;
+                    default:
+                        break;
+                }  break;
+            default:
+                break;
+        }
+      }
+        System.out.print("Введите месяц: ");
+        int month = scanner.nextInt();
+        System.out.println("Введите день: ");
+        int day = scanner.nextInt();
+        for (int i = 0; i<Weather.length;i++) {
+            if (i == month) {
+                switch (Weather.length) {
+                    case 30:
+                        for(int j = 0; j < 30; j++) {
+                            if(j == day) {
+                                System.out.println("Температура воздуха: " + Weather[i][j]);
+                            }
+                        }   break;
+                    case 31:
+                        for(int j = 0; j < 31; j++) {
+                            if (j == day) {
+                                System.out.println("Температура воздуха: " + Weather[i][j]);
+                            }
+                        }   break;
+                    case 29:
+                        for(int j = 0; j < 29; j++) {
+                            if (j == day) {
+                                System.out.println("Температура воздуха: " + Weather[i][j]);
+                            }
+                        }   break;
+                    default:
+                        break;
+                }
+            }
+       }
+        
+        int min = 50;
+        int max = 0;
+        for(int i = 0; i < Weather.length;i++) {
+            if(i == month) {
+                switch (Weather[i].length) {
+                    case 30:
+                        for(int j = 0; j < 30; j++) {
+                            if(Weather[i][j] > max) {
+                                max = Weather[i][j];
+                            }
+                            if(Weather[i][j] < min) {
+                                min = Weather[i][j];
+                            }
+                        }   break;
+                    case 31:
+                        for (int j = 0; j < 31; j++) {
+                            if (Weather[i][j] > max) {
+                                max = Weather[i][j];
+                            }
+                            if (Weather[i][j] < min) {
+                                min = Weather[i][j];
+                            }
+                        }   break;
+                    case 29:
+                        for (int j = 0; j < 29 ; j++) {
+                            if (Weather[i][j] > max) {
+                                max = Weather[i][j];
+                            }
+                            
+                            if (Weather[i][j] < min) {
+                                min = Weather[i][j];
+                            }
+                        }   break;
+                    default:
+                        break;
+                }
             }
         }
-  }
+        System.out.println("Самая высокая температура"+ max);
+        System.out.println("Самая низкая температура"+min);
+        
+        int avg = 0;
+        for (int i = 0; i<Weather.length; i++) {
+            System.out.println("Месяц: "+ i);
+        switch (Weather.length) {
+            case 30:
+                for(int j = 0; j < 30; j++) {
+                    avg = avg + Weather[i][j];
+                }
+                avg = avg / 30;
+                System.out.println("Среднее арифметическое: "+avg);
+                break;
+            case 31:
+                for(int j = 0; j < 31; j++) {
+                    avg = avg + Weather[i][j];
+                }
+                avg = avg / 31;
+                System.out.println("Среднее арифметическое: "+avg);
+                break;
+            case 29:
+                for(int j = 0; j < 29; j++) {
+                    avg = avg + Weather[i][j];
+                }
+                avg = avg / 29;
+                System.out.println("Среднее арифметическое: "+avg);
+                break;
+            default:
+                break;
+        }
+        }
+   }
 }
+
