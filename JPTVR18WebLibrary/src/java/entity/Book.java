3c8011a323ce639b960fadd047d0d94a92dc54f5
pupil <pupil@ivkhk.ee>
2020-01-14 11:17:28 +0200

    webLibrary

---
+++ JPTVR18WebLibrary/src/java/entity/Book.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author pupil
+ */
+public class Book implements Serializable{
+    private String name;
+    private String author;
+    private int publishedYear;
+    private String isbn;
+    
+    public Book() 
+    {
+        
+    }
+
+    public Book(String name, String author, int publishedYear, String isbn) {
+        this.name = name;
+        this.author = author;
+        this.publishedYear = publishedYear;
+        this.isbn = isbn;
+    }
+
+    
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPublishedYear() {
+        return publishedYear;
+    }
+
+    public void setPublishedYear(int publishedYear) {
+        this.publishedYear = publishedYear;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" + "name=" + name + ", author=" + author + ", publishedYear=" + publishedYear + ", isbn=" + isbn + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 89 * hash + Objects.hashCode(this.name);
+        hash = 89 * hash + Objects.hashCode(this.author);
+        hash = 89 * hash + this.publishedYear;
+        hash = 89 * hash + Objects.hashCode(this.isbn);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Book other = (Book) obj;
+        if (this.publishedYear != other.publishedYear) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.author, other.author)) {
+            return false;
+        }
+        if (!Objects.equals(this.isbn, other.isbn)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
---
+++ JPTVR18WebLibrary/src/java/entity/History.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ *
+ * @author pupil
+ */
+public class History implements Serializable {
+    private Date dateofissue;
+    private Date returnDate;
+    private Book book;
+    private Reader reader;
+    
+    public History() 
+    {
+        
+    }
+
+    public History(Date dateofissue, Date returnDate, Book book, Reader reader) {
+        this.dateofissue = dateofissue;
+        this.returnDate = returnDate;
+        this.book = book;
+        this.reader = reader;
+    }
+
+    public Date getDateofissue() {
+        return dateofissue;
+    }
+
+    public void setDateofissue(Date dateofissue) {
+        this.dateofissue = dateofissue;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public Reader getReader() {
+        return reader;
+    }
+
+    public void setReader(Reader reader) {
+        this.reader = reader;
+    }
+
+    @Override
+    public String toString() {
+        return "History{" + "dateofissue=" + dateofissue + ", returnDate=" + returnDate + ", book=" + book.getName() + ", "+ book.getAuthor() + ", reader=" + reader.getName() + ", " + reader.getSurname() +  '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 23 * hash + Objects.hashCode(this.dateofissue);
+        hash = 23 * hash + Objects.hashCode(this.returnDate);
+        hash = 23 * hash + Objects.hashCode(this.book);
+        hash = 23 * hash + Objects.hashCode(this.reader);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final History other = (History) obj;
+        if (!Objects.equals(this.dateofissue, other.dateofissue)) {
+            return false;
+        }
+        if (!Objects.equals(this.returnDate, other.returnDate)) {
+            return false;
+        }
+        if (!Objects.equals(this.book, other.book)) {
+            return false;
+        }
+        if (!Objects.equals(this.reader, other.reader)) {
+            return false;
+        }
+        return true;
+    }
+
+    public void SetReturnDate(Date date) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
+
